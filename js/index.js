0726ba5867e558fa97761e24d551dd4da40dc9ec
Shub Kale <skale856@gmail..com>
2020-06-14 14:49:09 +0530

    contactNav

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Nav bar toggle
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -12,8 +13,26 @@ navLinks.forEach(link => {
 })
 
 
+
+//Contact me toggle
 const ConTogg = document.querySelector('.ContactMe');
 
 ConTogg.addEventListener('click', () => {
         document.body.classList.toggle ('Cons-open')
     });
+
+  
+    
+//close contact list on , not on target 
+   /*         window.onload=function()
+            {
+                var hidelist=document.getElementById('conlist');
+                var ConMe=document.getElementById('conme')
+                document.onclick=function(d)
+                {
+                    if(d.target.id !=='conlist' && d.target.parentNode !=='conlist'  && d.target.id !=='conme'  && d.target.parentNode !=='conme')
+                    {
+                        document.body.classList.toggle ('Cons-open')    
+                    }
+                };
+            };      */
