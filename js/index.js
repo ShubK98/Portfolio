9d5936cff11e0cb3aded21c5f6c241055924cb4b
Shub Kale <skale856@gmail..com>
2020-10-04 13:27:32 +0530

    dashboard

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //Nav bar toggle
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -15,24 +15,44 @@ navLinks.forEach(link => {
 
 
 //Contact me toggle
+
 const ConTogg = document.querySelector('.ContactMe');
 
 ConTogg.addEventListener('click', () => {
-        document.body.classList.toggle ('Cons-open')
+        document.body.classList.toggle ('Cons-open');
+      
     });
 
-  
+ 
+    
     
 //close contact list on , not on target 
-   /*         window.onload=function()
+      /*      window.onload=function()
             {
                 var hidelist=document.getElementById('conlist');
                 var ConMe=document.getElementById('conme')
                 document.onclick=function(d)
-                {
+                {   
+                                        
                     if(d.target.id !=='conlist' && d.target.parentNode !=='conlist'  && d.target.id !=='conme'  && d.target.parentNode !=='conme')
                     {
-                        document.body.classList.toggle ('Cons-open')    
+                       
+                            document.body.classList.toggle ('Cons-close')   
+                       
+                        
                     }
+                                        
                 };
-            };      */
+            };      
+        */
+
+        //gsap animation
+     /*   const scroo = document.querySelector('.section__subtitle--intro')
+       
+
+        const tl = new TimelineMax();
+
+        tl.fromTo(scroo,2, {width: "20%"}, {width:"106%" })     */
+       
+
+        
